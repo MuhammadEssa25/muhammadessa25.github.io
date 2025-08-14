a231eec507a6869fee88ba86644c545a007bc06d
MohammadAbdullah1214 <essamemon234@gmail.com>
2025-08-14 19:44:25 +0500

    Build portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Enable static export
+  output: 'export',
+
+  // For GitHub Pages — make sure assets are loaded correctly
+  basePath: '/muhammadessa25.github.io',
+  assetPrefix: '/muhammadessa25.github.io/',
+
+  images: {
+    unoptimized: true, // Needed because GitHub Pages doesn't support Image Optimization
+  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
